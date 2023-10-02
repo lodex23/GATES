80dd09960a681d8f6ab4e1d86febdd726476fbb5
lodex23 <jeffrey.lodewyck23@gmail.com>
2023-10-02 15:55:30 +0200

    NAND_GATE_4

--- NAND_GATE.py
+++ NAND_GATE.py
@@ -1,7 +1,5 @@
 def NAND_GATE(a, b):
-    if a and b:      # als a en b True zijn
-        return 0     # return het tegenover gestelde
-    return 1         # als a en b False zijn return True
+    return not (a and b)
 
 def main():
     print(NAND_GATE(0, 0))
