9aaf13b5b19dfbd37f58a48f7950705d530be5aa
lodex23 <jeffrey.lodewyck23@gmail.com>
2023-10-02 14:48:01 +0200

    Slicer

---
+++ Slicen.py
@@ -0,0 +1,11 @@
+def main():
+    lijst = ['appel', 'peer', 'banaan']
+    # zoveelste value uit een lijst pakken
+    print(lijst[2])
+
+    # van de zoveelste waarde uit de lijst een character of meerdere pakken
+    print(lijst[2][0:2])
+
+
+if __name__ == "__main__":
+    main()
