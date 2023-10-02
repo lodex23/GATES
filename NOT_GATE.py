fc87c9a34f4e617e53b1659f89a304429f3d7a2f
lodex23 <jeffrey.lodewyck23@gmail.com>
2023-10-02 15:35:00 +0200

    Or, Not

---
+++ NOT_GATE.py
@@ -0,0 +1,9 @@
+def NOT_GATE(a):
+    return not a
+
+def main():
+    print(NOT_GATE(0))
+    print(NOT_GATE(1))
+
+if __name__ == "__main__":
+    main()
---
+++ OR_GATE.py
@@ -0,0 +1,14 @@
+# 1 = True and 0 = False
+
+def OR_GATE(a, b):
+    return a or b # return a | b kan ook!
+
+
+def main():
+    print(OR_GATE(0, 0))
+    print(OR_GATE(0, 1))
+    print(OR_GATE(1, 0))
+    print(OR_GATE(1, 1))
+
+if __name__ == "__main__":
+    main()
