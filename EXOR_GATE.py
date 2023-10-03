e559ead552ef1f58b280a4550879fee1e61e5e70
lodex23 <jeffrey.lodewyck23@gmail.com>
2023-10-03 09:25:51 +0200

    EXOR_GATE

---
+++ EXOR_GATE.py
@@ -0,0 +1,11 @@
+def EXOR_GATE(a, b):
+    return (a == b)
+
+def main():
+    print(EXOR_GATE(0, 0))
+    print(EXOR_GATE(0, 1))
+    print(EXOR_GATE(1, 0))
+    print(EXOR_GATE(1, 1))
+
+if __name__ == "__main__":
+    main()
